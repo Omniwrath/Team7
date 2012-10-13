fdddfd31b81d618287619fa9bca84409165f4525
Justin Ashburn <justinashburn07@gmail.com>
2012-10-12 22:56:26 -0700

    @Author request.

---
+++ src/bothellBirdMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Justin Ashburn, 2, 3, 4, 5, 6,7
+ *
+ */
+public class bothellBirdMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Please add your names to the author line.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
