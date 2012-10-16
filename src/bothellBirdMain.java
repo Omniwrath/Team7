50bc73929c3afd39ae15aca3209a0dac19fd0b1d
limestran <trano@u.washington.edu>
2012-10-15 23:40:18 -0700

    test commit

--- src/bothellBirdMain.java
+++ src/bothellBirdMain.java
@@ -3,7 +3,7 @@
  */
 
 /**
- * @author Justin Ashburn, 2, 3, 4, 5, 6,7
+ * @author Justin Ashburn, 2, 3, Oanh Tran, 5, 6,7
  *
  */
 public class bothellBirdMain {
@@ -14,7 +14,7 @@ public class bothellBirdMain {
 	public static void main(String[] args) {
 		System.out.println("Please add your names to the author line.");
 		// TODO Auto-generated method stub
-
+		int test; 
 	}
 
 }
