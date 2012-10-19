02dc611078d4025a9fa78aea49216a80eecced5d
limestran <trano@u.washington.edu>
2012-10-18 17:26:15 -0700

    bird class

---
+++ src/Bird.java
@@ -0,0 +1,50 @@
+
+public class Bird {
+	
+	 String name;
+	 String picture;
+	 String description;
+	 String sound;
+	 String recipe;
+	public Bird(String name, String picture, String description, String sound,
+			String recipe) {
+		super();
+		this.name = name;
+		this.picture = picture;
+		this.description = description;
+		this.sound = sound;
+		this.recipe = recipe;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSound() {
+		return sound;
+	}
+	public void setSound(String sound) {
+		this.sound = sound;
+	}
+	public String getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(String recipe) {
+		this.recipe = recipe;
+	}
+	
+
+}
