8441d5349a04e56556cece98b38780fb25fcbec5
Ahmed Brown <aiabgold@gmail.com>
2012-10-23 00:54:38 -0700

    Added whitespace

--- src/Bird.java
+++ src/Bird.java
@@ -1,11 +1,16 @@
+/*
+ * 
+ */
+
 
 public class Bird {
 	
-	 String name;
-	 String picture;
-	 String description;
-	 String sound;
-	 String recipe;
+	String name;
+	String picture;
+	String description;
+	String sound;
+	String recipe;
+	
 	public Bird(String name, String picture, String description, String sound,
 			String recipe) {
 		super();
@@ -15,36 +20,45 @@ public class Bird {
 		this.sound = sound;
 		this.recipe = recipe;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getPicture() {
 		return picture;
 	}
+	
 	public void setPicture(String picture) {
 		this.picture = picture;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
 	public String getSound() {
 		return sound;
 	}
+	
 	public void setSound(String sound) {
 		this.sound = sound;
 	}
+	
 	public String getRecipe() {
 		return recipe;
 	}
+	
 	public void setRecipe(String recipe) {
 		this.recipe = recipe;
 	}
 	
-
 }
