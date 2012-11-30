4c68268add37a14cc50aad8b5528b57a7e4ce5b8
Kaiswede <kaiswede@uw.edu>
2012-11-29 17:11:25 -0800

    Update src/BirdWhisperer.java
    Basic getNext() and getPrev() for birds.

--- src/BirdWhisperer.java
+++ src/BirdWhisperer.java
@@ -5,9 +5,23 @@ public class BirdWhisperer {
 		
 		
 	}
+	
+	public Bird getPrev(Bird currentBird) { //Gets the previous bird in the array
+			//Will change name to the binary search
+		index = findBird(currentBird); //Gets index from current bird
+		return birdcage[index - 1]; //Returns the bird in the index before
+		
+	}
+	
+	public Bird getNext(Bird currentBird) { //Gets next bird in the array
+			//Will change name to the binary search
+		index = findBird(currentBird); //Gets index from the current bird by finding it in the array
+		return birdcage[index + 1]; //Returns the next bird
+		
+	}
+
 
 	public Bird getBird() {
-		// TODO Auto-generated method stub
 		return null;// will return a bird, but return null for now
 		//called from the GUI
 		
